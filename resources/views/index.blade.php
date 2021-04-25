7949386d2b493b492c1c49b8806f95c41c8ac929
fdmk <faradinda96@gmail.com>
2021-04-25 20:40:11 +0700

    Menambahkan index.html

---
+++ resources/views/index.blade.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css" />
+
+    <title>hayVee</title>
+  </head>
+
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="img/logo.png" alt="" width="120" height="54" />
+          </a>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">hayPedia</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">hayDoc</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Testimoni</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown link
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    </nav>
+    <!-- Akhir Navbar -->
+
+    <!-- Jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+      </div>
+    </div>
+    <!-- Akhir Jumbotron -->
+
+    <!-- About -->
+    <div class="container">
+      <div class="row text-center mt-5">
+        <div class="col">
+          <h2>About Us</h2>
+        </div>
+      </div>
+
+      <div class="row text-center mt-3">
+        <div class="col">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam maiores nisi iusto dicta nesciunt repellendus odio, voluptates mollitia maxime neque, saepe est? Consequatur nobis, sunt fugiat harum deserunt saepe cum excepturi numquam expedita quibusdam corrupti, veritatis incidunt vitae inventore tempora sapiente quos blanditiis eaque voluptates. Odit, ut minima dolorem vero quae nesciunt repellat hic ratione!</p>
+        </div>
+      </div>
+    </div>
+    <!-- Akhir About -->
+
+    <!-- hayPedia -->
+    <section id="hayPedia">
+      <div class="container">
+        <div class="row text-center mt-5 mb-3">
+          <div class="col">
+            <h2>hayPedia</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-4 mb-3">
+            <div class="card mt-3">
+              <img src="img/hayPedia/dis1.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card mt-3">
+              <img src="img/hayPedia/dis1.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card mt-3">
+              <img src="img/hayPedia/dis1.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card mt-3">
+              <img src="img/hayPedia/dis1.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card mt-3">
+              <img src="img/hayPedia/dis1.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card mt-3">
+              <img src="img/hayPedia/dis1.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Akhir hayPedia -->
+
+    <!-- hayDoc -->
+    <section id="hayDoc">
+      <div class="container">
+        <div class="row text-center mt-5 mb-3">
+          <div class="col">
+            <h2>hayDoc</h2>
+          </div>
+        </div>
+ 
+        <div class="row justify-content-center">
+          <div class="col-md-4">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="img/hayPedia/dis1.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="img/hayPedia/dis2.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="img/hayPedia/dis3.jpg" class="d-block w-100" alt="...">
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Akhir hayDoc -->
+
+    <!-- Testimonial -->
+    <section id="testimonial">
+      <div class="container">
+        <div class="row text-center mt-5 mb-3">
+          <div class="col">
+            <h2>Testimonial</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-4 mb-3">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Akhir Testimonial -->
+
+    <!-- Footer -->
+    <footer class="bg-primary text-white">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, corporis.</p>
+    </footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+  </body>
+</html>
