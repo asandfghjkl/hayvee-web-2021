7e8f448d45f802cbb785d426408d2c8ef3027c69
fdmk <faradinda96@gmail.com>
2021-06-16 18:42:04 +0700

    Update index hayTalk

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -95,16 +95,29 @@
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/hayPedia/dis1.jpg" class="d-block w-100" alt="...">
+    <a href="{{ url('hayTalk') }}#hayTalk">
+    <img src="img/hayPedia/dis1.jpg" class="d-block w-100" alt="...">
+    </a>
+      
     </div>
     <div class="carousel-item">
-      <img src="img/hayPedia/dis1.jpg" class="d-block w-100" alt="...">
+    <a href="{{ url('hayTalk') }}#hayTalk">
+    <img src="img/hayPedia/dis1.jpg" class="d-block w-100" alt="...">
+    </a>
     </div>
     <div class="carousel-item">
-      <img src="img/hayPedia/dis1.jpg" class="d-block w-100" alt="...">
+    <a href="{{ url('hayTalk') }}#hayTalk">
+    <img src="img/hayPedia/dis1.jpg" class="d-block w-100" alt="...">
+    </a>
+    </div>
+    <div class="carousel-item">
+    <a href="{{ url('hayTalk') }}#hayTalk">
+    <img src="img/hayPedia/dis1.jpg" class="d-block w-100" alt="...">
+    </a>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
