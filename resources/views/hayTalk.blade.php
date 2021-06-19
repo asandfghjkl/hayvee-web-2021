4e4be756f6cfc316d8443e28ecd597335289db79
fdmk <faradinda96@gmail.com>
2021-06-19 23:50:57 +0700

    Fix card

--- resources/views/hayTalk.blade.php
+++ resources/views/hayTalk.blade.php
@@ -29,7 +29,7 @@
   <img src="img/hayPedia/dis1.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad placeat deleniti, odio officiis aut quos, possimus consequatur nam totam error adipisci nesciunt, omnis id fuga corrupti animi ducimus molestiae eveniet.</p>
   </div>
 </div>
 </div>
@@ -38,7 +38,7 @@
   <img src="img/hayPedia/dis1.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam rerum perspiciatis deserunt? Impedit iste esse illum atque voluptates eaque in vitae non assumenda dolorem cumque quos, inventore itaque placeat? Iste.</p>
   </div>
 </div>
 </div>
@@ -47,7 +47,7 @@
   <img src="img/hayPedia/dis1.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio hic nam, provident quod vitae saepe tempora alias aliquam cum esse dolore architecto voluptas accusantium, perferendis ut porro. Necessitatibus, cupiditate commodi!</p>
   </div>
 </div>
 </div>
@@ -56,7 +56,7 @@
   <img src="img/hayPedia/dis1.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae facere, magnam corrupti explicabo suscipit aliquid mollitia soluta enim, ipsa voluptate nulla, voluptatem omnis! Facilis libero a, quaerat consequuntur amet natus.</p>
   </div>
 </div>
 </div>
